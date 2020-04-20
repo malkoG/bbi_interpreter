c3815f6a51d98e119916b3e6f657feb64423ba23
malkoG <rijgndqw012@gmail.com>
2020-04-21 00:29:31 +0900

    Initialize Project

---
+++ lib/bbi_interpreter.rb
@@ -0,0 +1,6 @@
+require "bbi_interpreter/version"
+
+module BbiInterpreter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bbi_interpreter_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BbiInterpreter do
+  it "has a version number" do
+    expect(BbiInterpreter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
